ca3a143f5528d1c0032e3b5c020d751fc0c53e82
Pragati <101320941+Lalepragati@users.noreply.github.com>
2026-01-19 00:08:07 +0530

    Update EmailGeneratorService.java

--- email-writer-sb/src/main/java/com/email/email_writer_sb/EmailGeneratorService.java
+++ email-writer-sb/src/main/java/com/email/email_writer_sb/EmailGeneratorService.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import java.util.Map;
-
 @Service   // help us calling api call
 public class EmailGeneratorService {
     private final WebClient webClient;
