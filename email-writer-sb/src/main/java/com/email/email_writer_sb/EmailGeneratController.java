4a78058d5254277568e1ccf1de4136df72ef3fa6
Pragati <101320941+Lalepragati@users.noreply.github.com>
2026-01-22 22:54:04 +0530

    Add annotations to EmailGeneratController

--- email-writer-sb/src/main/java/com/email/email_writer_sb/EmailGeneratController.java
+++ email-writer-sb/src/main/java/com/email/email_writer_sb/EmailGeneratController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 //Annotation
 @RestController
+    
 @AllArgsConstructor
 @CrossOrigin(origins="*")
 @RequestMapping("/api/email")
