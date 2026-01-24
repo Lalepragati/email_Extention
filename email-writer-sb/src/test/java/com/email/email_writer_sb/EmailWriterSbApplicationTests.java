a9827ab2e68d6e87516e066a59873acd299e9193
Pragati <101320941+Lalepragati@users.noreply.github.com>
2026-01-25 01:09:09 +0530

    Add empty line for code style in test class

--- email-writer-sb/src/test/java/com/email/email_writer_sb/EmailWriterSbApplicationTests.java
+++ email-writer-sb/src/test/java/com/email/email_writer_sb/EmailWriterSbApplicationTests.java
@@ -3,6 +3,7 @@ package com.email.email_writer_sb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
+	
 class EmailWriterSbApplicationTests {
 	@Test
 	void contextLoads() {
