71414660c0f29a2c97f7f9ff8f42517cda64a763
Pragati <101320941+Lalepragati@users.noreply.github.com>
2026-01-25 21:22:23 +0530

    Fix formatting of SpringBootTest annotation

--- email-writer-sb/src/test/java/com/email/email_writer_sb/EmailWriterSbApplicationTests.java
+++ email-writer-sb/src/test/java/com/email/email_writer_sb/EmailWriterSbApplicationTests.java
@@ -2,8 +2,7 @@ package com.email.email_writer_sb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
-	
+@SpringBootTest	
 class EmailWriterSbApplicationTests {
 	@Test
 	void contextLoads() {
