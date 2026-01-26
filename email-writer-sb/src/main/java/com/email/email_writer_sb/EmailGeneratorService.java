9f443ff95387989b3c894c53b6efd192ec4f0b19
Pragati <101320941+Lalepragati@users.noreply.github.com>
2026-01-27 00:42:31 +0530

    Update EmailGeneratorService.java

--- email-writer-sb/src/main/java/com/email/email_writer_sb/EmailGeneratorService.java
+++ email-writer-sb/src/main/java/com/email/email_writer_sb/EmailGeneratorService.java
@@ -8,7 +8,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.util.Map;
 @Service   // help us calling api call
 public class EmailGeneratorService {
-    
     private final WebClient webClient;  
     @Value("${gemini.api.url}")
     private String geminiApiUrl;
