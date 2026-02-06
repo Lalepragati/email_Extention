f6ccc809782db5669958fd7a496bec2d6f06cf91
Pragati <101320941+Lalepragati@users.noreply.github.com>
2026-02-06 21:55:21 +0530

    Remove unnecessary blank line in test class

--- email-writer-sb/src/test/java/com/email/email_writer_sb/EmailWriterSbApplicationTests.java
+++ email-writer-sb/src/test/java/com/email/email_writer_sb/EmailWriterSbApplicationTests.java
@@ -2,7 +2,6 @@ package com.email.email_writer_sb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest	
-	
 class EmailWriterSbApplicationTests {
 	@Test
 	void contextLoads() {
