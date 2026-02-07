2e55d4050167b829b84894e38b6208879cf7662e
Pragati <101320941+Lalepragati@users.noreply.github.com>
2026-02-07 22:03:24 +0530

    Remove unnecessary whitespace in EmailGeneratController

--- email-writer-sb/src/main/java/com/email/email_writer_sb/EmailGeneratController.java
+++ email-writer-sb/src/main/java/com/email/email_writer_sb/EmailGeneratController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 //Annotation
 @RestController  
-    
 @AllArgsConstructor
 @CrossOrigin(origins="*")
 @RequestMapping("/api/email")
